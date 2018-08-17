cfe30ee270ea891d6bf8fb64373bf9f6bebb20ef
pavalex <Alex.P79@mail.ru>
2018-08-18 00:06:36 +0800

    viewed, notification

--- dist/js/main.js
+++ dist/js/main.js
@@ -10,4 +10,15 @@ $(document).ready(function() {
     prevArrow: '<div class="slider-arrow slider-arrow__left"></div>',
     nextArrow: '<div class="slider-arrow slider-arrow__right"></div>'
   });
+
+  $(".viewed-slider").slick({
+    arrows: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    prevArrow:
+      '<div class="viewed-slider-arrow viewed-slider-arrow__left-normal"></div>',
+    nextArrow:
+      '<div class="viewed-slider-arrow viewed-slider-arrow__right-normal"></div>'
+  });
 });

8e7634e28ae6f40addf5b096fead88f48016c392
pavalex <Alex.P79@mail.ru>
2018-08-17 11:30:17 +0800

    fix-header, fix-slider, popular

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,13 @@
-new WOW().init();
+// new WOW().init();
 
-
-
+$(document).ready(function() {
+  /* slick */
+  $(".slider-bg").slick({
+    arrows: true,
+    dots: true,
+    autoplay: false,
+    swipeToSlide: true,
+    prevArrow: '<div class="slider-arrow slider-arrow__left"></div>',
+    nextArrow: '<div class="slider-arrow slider-arrow__right"></div>'
+  });
+});

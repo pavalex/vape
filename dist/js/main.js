f62c7661e9e396c8b06b6fd86e8364dc68c73b3d
pavalex <Alex.P79@mail.ru>
2018-08-17 12:37:08 +0800

    discounts

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,11 @@
-// new WOW().init();
+new WOW().init();
 
 $(document).ready(function() {
   /* slick */
   $(".slider-bg").slick({
     arrows: true,
     dots: true,
-    autoplay: false,
+    autoplay: true,
     swipeToSlide: true,
     prevArrow: '<div class="slider-arrow slider-arrow__left"></div>',
     nextArrow: '<div class="slider-arrow slider-arrow__right"></div>'

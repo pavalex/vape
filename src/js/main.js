9f4ad46a3c9a0f4afa2bba71ea7538f6ff0869a6
pavalex <Alex.P79@mail.ru>
2018-08-19 01:19:35 +0800

    modal collback, modal basket

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,51 @@
 new WOW().init();
 
 $(document).ready(function() {
+  /* modal-callback */
+  $(".header-middle-btn").on("click", function() {
+    $(".overlay").fadeIn(400);
+  });
+
+  $(".footer-contacts-btn").on("click", function() {
+    $(".overlay").fadeIn(400);
+  });
+
+  $(".callback-close").on("click", function() {
+    $(".overlay").fadeOut(400);
+  });
+
+  $(".overlay").click(function() {
+    $(document).on("click", function(event) {
+      var select = $(".callback");
+      if ($(event.target).closest(select).length) return;
+      $(".overlay").fadeOut(400);
+      $(document).unbind("click");
+      event.stopPropagation();
+    });
+  });
+
+  /* basket-modal */
+  $(".header-middle-basket-block__subtitle").on("click", function() {
+    $(".overlay-basket").fadeIn(400);
+  });
+
+  $(".basket-modal-close").on("click", function() {
+    $(".overlay-basket").fadeOut(400);
+  });
+
+  $(".overlay-basket").click(function() {
+    $(document).on("click", function(event) {
+      var select = $(".basket-modal");
+      if ($(event.target).closest(select).length) return;
+      $(".overlay-basket").fadeOut(400);
+      $(document).unbind("click");
+      event.stopPropagation();
+    });
+  });
+
+  /* jquery.maskedinput */
+  $(".callback-form-input__tel").mask("+99 (999) 999 99 99");
+
   /* slick */
   $(".slider-bg").slick({
     arrows: true,
